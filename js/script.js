86c8152fe792bbc52afc7b51f89328de49e27154
Syriaca <jean-yves.lecabellec@leetchi.com>
2018-01-24 21:29:19 +0100

    Step 1
    Added quotes array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotes = [
+    {
+        quote: "La vieillesse. C’est la seule maladie dont on ne peut espérer guérir.",
+        source: "Citizen Kane"
+    },
+    {
+        quote: "La vie c'est comme une boîte de chocolats, on ne sait jamais sur quoi on va tomber.",
+        source: "Forrest Gump"
+    },
+    {
+        quote: "Un homme qui ne passe pas de temps avec sa famille n’est pas vraiment un homme.",
+        source: "Le parrain"
+    },
+    {
+        quote: "La capote, c'est le soulier de verre de notre génération. On l'enfile quand on rencontre une inconnue, on danse toute la nuit, et puis on la balance.",
+        source: "Fight Club"
+    },
+    {
+        quote: "C'est à moi que tu parles",
+        source: "Taxi Driver"
+    },
+];
